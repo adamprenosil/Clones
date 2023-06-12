bab8f67c3599ea364f655d141437950111ed4539
Gonzalo Zigarán <gjzigaran@gmail.com>
2023-06-12 18:06:32 -0300

    hask y operadores para BR

--- binary_relation.py
+++ binary_relation.py
@@ -1,5 +1,6 @@
 
 import numpy
+from itertools import chain
 from folpy.utils import indent
 
 
@@ -13,6 +14,8 @@ class BinaryRelation(object):
     def __init__(self, universe, pairs=None, matrix=None, name=""):
         assert not isinstance(universe, int)
         assert isinstance(universe, list), "El universo debe ser una lista"
+        self.universe = sorted(universe)
+        self.universe_card = len(self.universe)
         assert pairs or matrix, "la relación hay que cargarla por la lista de pares o la matriz"
         if matrix:
             self.matrix = matrix
@@ -23,11 +26,8 @@ class BinaryRelation(object):
                                       dtype=bool)
             for (i,j) in pairs:
                 self.matrix[i,j] = True
-        self.universe = sorted(universe)
-        self.universe_card = len(self.universe)
         self.name = name
         self.class_name = type(self).__name__
-    
 
     def __repr__(self):
         if self.name:
@@ -37,6 +37,13 @@ class BinaryRelation(object):
             result += indent(repr(self.universe) + ",\n")
             result += indent(repr(self.matrix) + ",\n")
             return result + ")"
+
+    def __hash__(self):
+        """
+        Hash para las relaciones binarias
+        """
+        return hash(frozenset(chain(self.universe,
+                                    str(self.matrix))))
     
     def __eq__(self, other):
         if self.universe != other.universe:
@@ -51,6 +58,24 @@ class BinaryRelation(object):
         'A==B no implica !(A!=B)'
         """
         return not self.__eq__(other)
+
+    def __mul__(self, other):
+        """
+        La relacion resultante de intersecar self con other
+        """
+        return self.intersection(self, other)
+
+    def __matmul__(self, other):
+        """
+        La relacion resultante de componer self con other
+        """
+        return self.compose(self, other)
+
+    def __and__(self, other):
+        """
+        La relacion resultante de intersecar self con other
+        """
+        return self.intersection(self, other)
 
     def T(self):
         """
@@ -80,3 +105,24 @@ def top_relation(universe):
 def bottom_relation(universe):
     pairs = [(a,a) for a in universe]
     return BinaryRelation(universe, pairs=pairs)
+
+# def rel_clousure(rel):
+#     """
+#     Obtiene el conjunto de relaciones binarias que es la clausura de `rel` con 
+#     las operaciones T, @ y *
+#     """
+#     universe = rel.universe
+#     clausure_set = {bottom_relation(universe), top_relation(universe), rel}
+#     new_rels = clausure_set.copy()
+#     new_new_rels = set()
+
+#     while(new_rels != set()):
+#         for old_rel in clausure_set:
+#             for new_rel in new_rels:
+#                 intersection = old_rel * new_rel
+#                 composition = old_rel @ new_rel
+#                 new_new_rels.add(intersection)
+#                 new_new_rels.add(composition)
+
+
+#     return clausure_set
