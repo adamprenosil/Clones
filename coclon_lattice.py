2eed59bf99c25668eeb8174bd28e70115f693400
Gonzalo Zigarán <gjzigaran@gmail.com>
2023-09-07 18:41:30 -0300

    genera el reticulado de una lista de coclones y grafica

---
+++ coclon_lattice.py
@@ -0,0 +1,27 @@
+
+from folpy.semantics.lattices import Lattice
+from folpy.semantics.modelfunctions import Operation_decorator
+
+def gen_coclon_lattice(coclones):
+    """
+    Genera el modelo de reticulado a partir de la lista de los coclones como
+    conjuntos.
+    """
+    universe = range(len(coclones))
+
+    @Operation_decorator(universe)
+    def meet(c1,c2):
+        return coclones.index(coclones[c1].intersection(coclones[c2]))
+    
+    @Operation_decorator(universe)
+    def join(c1,c2):
+        union = coclones[c1].union(coclones[c2])
+        up_union = [x for x in coclones if union.issubset(x)]
+        result = min(up_union)
+        return coclones.index(result)
+
+    return Lattice(universe, join, meet)
+
+def draw_coclon_lattice(coclones):
+    lattice = gen_coclon_lattice(coclones)
+    return lattice.draw()
